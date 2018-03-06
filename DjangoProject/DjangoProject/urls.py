46ee6e93748f4e6ba50f2109bbed31e3fc30b03b
Losh_lo <alaaalshamy44@yahoo.com>
2018-03-07 01:44:22 +0200

    models

--- DjangoProject/DjangoProject/urls.py
+++ DjangoProject/DjangoProject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from liberary import  views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
+    path('books/', views.book_view, name='books'),
+    path('books/books/<int:pk>', views.book_detail, name='book-detail'),
 ]
--- DjangoProject/liberary/models.py
+++ DjangoProject/liberary/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 class Writer(models.Model):
     witer_name = models.CharField(max_length=200)
-    date_birth= models.DateTimeField('date Of Birth')
-    date_death=models.DateTimeField('date of Death')
-    Contact=models.URLField("Enter URL")
-    Bio=models.TextField("About")
+    date_birth= models.DateTimeField(default='')
+    date_death=models.DateTimeField(default='')
+    Contact=models.URLField(blank=True)
+    Bio=models.TextField(blank=True)
 
 
 class Book(models.Model):
-    Book_name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    date_publish= models.DateTimeField(default='')
+    sammary=models.TextField(blank=True)
+    country= models.TextField(blank=True)
+    link = models.URLField(blank=True)
+  #  authors = models.ManyToOneRel(Writer,field_name="witer_name")
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering =['title']
