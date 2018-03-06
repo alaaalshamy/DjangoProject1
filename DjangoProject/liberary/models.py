6d559c6de99185ae0c46becd957edf0695b36043
Losh_lo <alaaalshamy44@yahoo.com>
2018-03-06 17:32:25 +0200

    models

--- DjangoProject/liberary/models.py
+++ DjangoProject/liberary/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 class Writer(models.Model):
     witer_name = models.CharField(max_length=200)
-    date_birth= models.DateTimeField('date published')
+    date_birth= models.DateTimeField('date Of Birth')
+    date_death=models.DateTimeField('date of Death')
+    Contact=models.URLField("Enter URL")
+    Bio=models.TextField("About")
+    Books=models.ManyToOneRel()
 
+
+class Book(models.Model):
+    Book_name = models.CharField(max_length=200)
