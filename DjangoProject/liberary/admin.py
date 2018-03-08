5eaf4b57421a0445d2e423f9ed4ab8387efd3378
Losh_lo <alaaalshamy44@yahoo.com>
2018-03-08 15:13:11 +0200

    models

--- DjangoProject/liberary/admin.py
+++ DjangoProject/liberary/admin.py
@@ -1,11 +1,2 @@
 from django.contrib import admin
-# from .models import  Book
-# from .models import  Writer
-#
-#
-#
-# admin.site.register(Book)
-# admin.site.register(Writer)
 
-
-# Register your models here.
--- DjangoProject/liberary/models.py
+++ DjangoProject/liberary/models.py
@@ -1,17 +1,5 @@
 from django.db import models
 
-class Writer(models.Model):
-
-    witer_name = models.CharField(max_length=200)
-    # date_birth= models.DateTimeField(default='')
-    # date_death=models.DateTimeField(default='')
-    # Contact=models.URLField(blank=True)
-    # Bio=models.TextField(blank=True)
-    #
-    def __str__(self):
-        return self.witer_name
-
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     date_publish= models.DateTimeField(default='')
